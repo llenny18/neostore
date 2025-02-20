9f9f5669beb7653f543e77bf6b6e81bcff295855
llenny18 <ramosalleneid01@gmail.com>
2025-02-20 11:40:06 +0800

    remake

--- neostore/wsgi.py
+++ neostore/wsgi.py
@@ -1,5 +1,8 @@
+
 import os
+
 from django.core.wsgi import get_wsgi_application
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_blog.settings")
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trymuna.settings')
+
 application = get_wsgi_application()
-app = application
